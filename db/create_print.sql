9f8fc13b5bb3292e7497a7bb20ae1f23de72fee8
nabiladigo <nabiladigo91@gmail.com>
2022-02-12 13:58:32 -0500

    create user

---
+++ db/create_print.sql
@@ -0,0 +1,16 @@
+INSERT INTO main_app_print(name, image, price, verified_print, created_at)
+  VALUES
+  ('Mugs','https://i.ebayimg.com/images/g/VAQAAOSwVj5bW9YZ/s-l400.jpg','5','f', '02-08-2022');
+
+INSERT INTO main_app_print(name, image, price, verified_print, created_at)
+  VALUES
+  ('Cards','https://pinkandposh.co.uk/wp-content/uploads/2022/01/Handmade-Valentines-Card-Christian-Louboutin-Style-scaled-440x440.jpg', '10', 'f', '02-08-2022');
+
+INSERT INTO main_app_print(name, image, price, verified_print, created_at)
+  VALUES
+  ('Photo Books','https://www.prestophoto.com/storage/static/landing/photo-books/open-photo-book.png', '20', 'f', '02-08-2022');
+
+INSERT INTO main_app_print(name, image, price, verified_print, created_at)
+  VALUES
+  ('Puzzles','https://assets.entrepreneur.com/content/3x2/2000/1641591044-Ent-Milliondollarpuzzle.jpeg', '15', 'f', '02-08-2022')
+ 
