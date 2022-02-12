04ea6b5dd0e1e215b7467eb7974c721dda3a55f6
nabiladigo <nabiladigo91@gmail.com>
2022-02-12 13:33:45 -0500

    create modles and urls

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Print(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    price = models.TextField(max_length=500)
+    verified_print = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Card(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    price = models.IntegerField(default=0)
+    print = models.ForeignKey(Print, on_delete= models.CASCADE, related_name= "cards")
+    
+    def __str__(self):
+        return self.name
+     
+class Mug(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    price = models.IntegerField(default=0)
+    print = models.ForeignKey(Print, on_delete= models.CASCADE, related_name= "mugs")
+    
+    def __str__(self):
+        return self.name
+     
+class Photo(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    price = models.IntegerField(default=0)
+    print = models.ForeignKey(Print, on_delete= models.CASCADE, related_name= "photos")
+    
+    def __str__(self):
+        return self.name
+     
+class Puzzle(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    price = models.IntegerField(default=0)
+    print = models.ForeignKey(Print, on_delete= models.CASCADE, related_name= "puzzles")
+    
+    def __str__(self):
+        return self.name
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path 
 from . import views
 
-urlpatterns =[
+urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('prints/', views.PrintList.as_view(), name ="print_list"),
+    path('prints/new/',  views.PrintCreate.as_view(), name="print_create"),
+    path('prints/<int:pk>/', views.PrintDetail.as_view(), name="print_detail"),
+    path('prints/<int:pk>/update',views.PrintUpdate.as_view(), name="print_update"),
+    path('prints/<int:pk>/delete',views.PrintDelete.as_view(), name="print_delete"), 
+    path('prints/<int:pk>/cards/new/', views.CardCreate.as_view(), name="card_create"),
+    path('prints/<int:pk>/mugs/new/', views.MugCreate.as_view(), name="mug_create"),
+    path('prints/<int:pk>/photos/new/', views.PhotoCreate.as_view(), name="photo_create"),
+    path('prints/<int:pk>/puzzles/new/', views.PuzzleCreate.as_view(), name="puzzle_create"),
+
 ]
